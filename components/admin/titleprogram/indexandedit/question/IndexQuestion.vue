5989cb291f3df3795f7330bf92db04e265188185
zedparman <ze.dparman@gmail.com>
2022-06-09 10:23:57 +0430

    address id done

--- components/admin/titleprogram/indexandedit/question/IndexQuestion.vue
+++ components/admin/titleprogram/indexandedit/question/IndexQuestion.vue
@@ -3,23 +3,27 @@
     <h4>مشاده سوالات مربوط به یک رژیم با ارسال آی دی رژیم در نوارد ادرس   14</h4>
 
 
-    <ul>
-      <li v-for="doc in docs" :key="doc.id" class="d-flex">
-        <!-- <h1>{{ docs.data.data.mobile }}</h1> -->
-        <p class="m-2">{{ doc.title_question }}</p><br>
-        <!-- <p class="m-2">{{ doc.options_question }}</p> -->
-      </li>
-    </ul>  
 
+  <form @submit.prevent="getDoc">
+    <input type="text" v-model="title_program_id">
+    <button type="subnit">submit</button>
+  </form>
+  <div v-for="doc in docs.data" :key="doc.id">
+    {{ doc.title_program_id }}
+    {{ doc.title_question }}
+  </div>
 
   </div>
 </template>
 
 <script>
+import questioncomponent from './questioncomponent.vue';
 export default {
+  components: { questioncomponent },
   data() {
     return {
-      docs: []
+      docs: [],
+      "title_program_id": ""
     };
   },
   mounted() {
@@ -28,15 +32,14 @@ export default {
   methods: {
     async getDoc() {
       let res = await this.$axios.get(
-        "http://95.217.96.131:8080/api/admin/index-question/37",
+        `http://95.217.96.131:8080/api/admin/index-question/${this.title_program_id}`,
         {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
           },
         }
       );
-            this.docs = res.data.data;
-            console.log(res.data);
+            this.docs = res.data;
     },
   },
 }
---
+++ components/admin/titleprogram/indexandedit/question/questioncomponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="doc in docss.data" :key="doc.id" >
+            <!-- <button @click="$emit('confirmdoc', doc.expert_id, doc.result)">x</button> -->
+            <!-- <p class="m-2">{{ doc.data }}</p> -->
+            <h4>{{ doc.title_program_id }}</h4>
+            <!-- <p class="m-2">{{ doc.description }}</p> -->
+            <!-- <p class="m-2">{{ doc.result }}</p> -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['docss']
+    }
+        // console.log(doc.data)
+</script>
+
+<style scoped>
+
+</style>
